7e72ee66f7fd79c6042e292e822cc89a5a4ae709
Jasper Bekkers <Jasper-Bekkers@users.noreply.github.com>
2021-12-04 17:42:09 +0100

    Update gpg-loopback.bat

--- gpg-loopback.bat
+++ gpg-loopback.bat
@@ -1,2 +1,3 @@
-REM git config --global gpg.program "C:/Scripts/gpg-loopback.bat"
+@echo off
+rem git config --global gpg.program "C:/Scripts/gpg-loopback.bat"
 "C:\Program Files\Git\usr\bin\gpg" --pinentry-mode loopback %*
